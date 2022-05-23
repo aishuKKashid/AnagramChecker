b166e7cb92c62f16e372795f7daa5bb8d4744428
Aishwarya Kashid <aishwaryakashid@Administrators-MacBook-Pro-4.local>
2022-05-23 14:12:39 +0530

    Add unit tests

--- anagram-checker/src/AnagramChecker.kt
+++ anagram-checker/src/AnagramChecker.kt
@@ -2,9 +2,10 @@ class AnagramChecker {
     fun match(word1: String, word2: String): Boolean {
         val counter = IntArray(26) { 0 }
         if (word1.length != word2.length) return false
+//        if (word1.isEmpty() || word2.isEmpty()) return false
         (word1.indices).forEach { index ->
-            ++counter[word1[index] - 'a']
-            --counter[word2[index] - 'a']
+            ++counter[word1[index].lowercaseChar() - 'a']
+            --counter[word2[index].lowercaseChar() - 'a']
         }
         return !counter.any { it != 0 }
     }
--- anagram-checker/test/AnagramCheckerTest.kt
+++ anagram-checker/test/AnagramCheckerTest.kt
@@ -1,10 +1,30 @@
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldHave
 
 class AnagramCheckerTest : StringSpec() {
     init {
         "is 'a' an anagram of 'a'" {
             AnagramChecker().match("a", "a").shouldBeTrue()
+        }
+        "is 'a' not an anagram of 'b'" {
+            AnagramChecker().match("a", "b").shouldBeFalse()
+        }
+        "is 'dog' an anagram of 'god'" {
+            AnagramChecker().match("dog", "god").shouldBeTrue()
+        }
+        " is 'abcd' is not an anagram of 'bs'" {
+            AnagramChecker().match("abcd", "bc").shouldBeFalse()
+        }
+        "should not have empty string inputs" {
+            AnagramChecker().match("", "").shouldBeFalse()
+        }
+        "should match the a and 'A'" {
+            AnagramChecker().match("a", "A").shouldBeTrue()
+        }
+        "is 'a' is not an anagram of 'aa'" {
+            AnagramChecker().match("a", "aaa").shouldBeFalse()
         }
     }
 }
